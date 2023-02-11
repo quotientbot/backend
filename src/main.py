9a1b6ec4ba38cef3b15b05cdf3a6567da092ef46
deadaf <gahlyanrohit18@gmail.com>
2023-02-11 23:48:26 +0530

    authentication & routes setup

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Depends, HTTPException
+from decouple import config
+from fastapi.security import APIKeyHeader
+from routes import auth
+
+api_scheme = APIKeyHeader(name="authorization")
+
+
+async def verify_key(key: str = Depends(api_scheme)):
+    if key != config("API_KEY"):
+        raise HTTPException(status_code=403)
+
+
+app = FastAPI(debug=True)
+app.include_router(auth.router, prefix="/auth", dependencies=[Depends(verify_key)])
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host=config("API_HOST"), port=int(config("API_PORT")))
