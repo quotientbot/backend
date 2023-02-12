aa4e59d6b55f98d7f51fa1bfc6ea1717d52363c9
deadaf <gahlyanrohit18@gmail.com>
2023-02-12 19:38:38 +0530

    update user route to fetch user's guilds as well

--- src/routes/user.py
+++ src/routes/user.py
@@ -38,10 +38,23 @@ async def get_user_from_discord(access_token: str):
             return await response.json()
 
 
+async def get_user_guilds_from_discord(access_token: str):
+    """
+    Use the access token to fetch user's guilds from discord.
+    """
+    headers = {"Authorization": "Bearer {}".format(access_token)}
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            config("DISCORD_API") + "/users/@me/guilds", headers=headers
+        ) as response:
+            return await response.json()
+
+
 @router.get("/@me")
 async def get_user(code: str):
     """
-    Get the user's data from discord.
+    Get the user's data & guilds from discord.
     """
     response = await exchange_code(code)
     access_token = response.get("access_token")
@@ -50,4 +63,6 @@ async def get_user(code: str):
         return response
 
     user = await get_user_from_discord(access_token)
+    guilds = await get_user_guilds_from_discord(access_token)
+    user["guilds"] = guilds
     return user
