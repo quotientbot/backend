2a957cac3e5cb426df43d63a6d5f89d107ee535a
deadaf <gahlyanrohit18@gmail.com>
2023-02-12 23:23:57 +0530

    add cors middleware

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends, HTTPException
 from decouple import config
 from fastapi.security import APIKeyHeader
 from routes import auth, user
+from fastapi.middleware.cors import CORSMiddleware
 
 api_scheme = APIKeyHeader(name="authorization")
 
@@ -16,6 +17,19 @@ app = FastAPI(debug=True)
 # dependencies=[Depends(verify_key)]
 app.include_router(auth.router, prefix="/auth")
 app.include_router(user.router, prefix="/user")
+
+
+origins = [
+    "*",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 if __name__ == "__main__":
     import uvicorn
